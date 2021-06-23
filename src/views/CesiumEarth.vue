989865c85f896b4150fdcb06a17bf1d36192ed0b
huhaibiao <huhaibiao@viaspace.cn>
2021-06-23 21:34:33 +0800

    geng该

--- src/views/CesiumEarth.vue
+++ src/views/CesiumEarth.vue
@@ -13,22 +13,23 @@
 <script>
 // import * as Cesium from "cesium";
 // import "cesium/Build/Cesium/Widgets/widgets.css";
+import { onMounted } from 'vue';
 export default {
   name: "CesiumEarth",
     setup() {
         function getCesium(){
             // window.CESIUM_BASE_URL = '/static/Cesium/';
-          //  let viewer = new Cesium.Viewer('cesiumContainer');
-          //   console.log(viewer);
-            var viewer = new Cesium.Viewer('cesiumContainer', {
-    animation: true, //是否显示动画控件(左下方那个)
-    baseLayerPicker: true, //是否显示图层选择控件
-    geocoder: true, //是否显示地名查找控件
-    timeline: true, //是否显示时间线控件
-    sceneModePicker: true, //是否显示投影方式控件
-    navigationHelpButton: false, //是否显示帮助信息控件
-    infoBox: true, //是否显示点击要素之后显示的信息
-});
+           let viewer = new Cesium.Viewer('cesiumContainer');
+            console.log(viewer);
+//             var viewer = new Cesium.Viewer('cesiumContainer', {
+//     animation: true, //是否显示动画控件(左下方那个)
+//     baseLayerPicker: true, //是否显示图层选择控件
+//     geocoder: true, //是否显示地名查找控件
+//     timeline: true, //是否显示时间线控件
+//     sceneModePicker: true, //是否显示投影方式控件
+//     navigationHelpButton: false, //是否显示帮助信息控件
+//     infoBox: true, //是否显示点击要素之后显示的信息
+// });
         }
         return {
             getCesium,
